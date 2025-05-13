c81aa72cd59aca9dcc83ab68c073c7410b581a2d
Meet Boghani <meetcboghani02@gmail.com>
2025-05-13 00:37:22 -0400

    Feat: changes for database.

--- Todos/database.py
+++ Todos/database.py
@@ -7,11 +7,19 @@ from sqlalchemy.ext.declarative import declarative_base
 This is used to connect database with local database which is sqlite3.
 """
     # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-"""
-This crededbtials is to connect postgresql which is server based swl database. 
-"""
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/TodoApplicationDatabase'
+    # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL) # connect_args={'check_same_thread': False} this parameter is used only for sqlite3
+"""
+This crededbtials is to connect postgresql which is server based sql database. 
+"""
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+"""
+This crededbtials is to connect postgresql on render as Paas(project as a service).
+"""
+SQLALCHEMY_DATABASE_URL = 'postgresql://todoapplicationdatabase_99sq_user:yvpq5ff30UnWhk9ic0uHBEkffpFDqjNn@dpg-d0hcmkidbo4c73dld8u0-a.oregon-postgres.render.com/todoapplicationdatabase_99sq'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
